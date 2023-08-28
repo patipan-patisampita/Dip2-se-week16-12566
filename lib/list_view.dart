ed7edcec366a42c8cf55f81e51a2f090af2a53fc
patipan-patisampita <patipan-patisampita@gmail.com>
2023-08-28 14:55:40 +0700

    update

---
+++ lib/list_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ListViewDemo extends StatelessWidget {
+  const ListViewDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 200,
+              color: Colors.deepPurple[200],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
